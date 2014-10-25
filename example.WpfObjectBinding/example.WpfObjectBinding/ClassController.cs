e018ff82429b936cc2a1707bb1e5aa2c13c997a1
Michael Millies <mmillies@gmail.com>
2014-10-25 16:30:28 -0500

    A rewrite of the object data binding example. There may be some differences to the original blog post.

---
+++ example.WpfObjectBinding/example.WpfObjectBinding/ClassController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example.WpfObjectBinding
+{
+    public class ClassController
+    {
+        #region Private Members
+
+        private readonly ClassInfo[] _data = new ClassInfo[] {
+            new ClassInfo() { Name = "Calculus II" },
+            new ClassInfo() { Name = "Physics" },
+            new ClassInfo() { Name = "AI Algorithms and Design Considerations" },
+            new ClassInfo() { Name = "Programming Patterns" },
+            new ClassInfo() { Name = "Computer Networks" },
+            new ClassInfo() { Name = "Building and Programming Robots" },
+            new ClassInfo() { Name = "Distributed Systems" }
+        };
+
+        private ClassModel _model;
+
+        #endregion
+
+        public void Initialize(ClassModel model)
+        {
+            this._model = model;
+
+            // The original writing of the how-to apparently didn't have the IEnumerable overload?
+            this._model.Classes = new System.Collections.ObjectModel.ObservableCollection<ClassInfo>(this._data);
+        }
+    }
+}
---
+++ example.WpfObjectBinding/example.WpfObjectBinding/ClassModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example.WpfObjectBinding
+{
+    public class ClassModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ClassInfo> _classes;
+        public ObservableCollection<ClassInfo> Classes
+        {
+            get { return this._classes; }
+            set
+            {
+                this._classes = value;
+
+                // The name of the property so the WPF framework updates any bindings referring to it.
+                this.sendPropertyChanged("Classes");    
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void sendPropertyChanged(string property)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        #endregion
+    }
+}
